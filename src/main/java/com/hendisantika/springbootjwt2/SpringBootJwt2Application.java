cbf7d1313549d4f61c3924768608279129b39a59
Hendi Santika <hendisantika@gmail.com>
2018-02-27 06:03:03 +0700

    #11 - Modify Main App

--- src/main/java/com/hendisantika/springbootjwt2/SpringBootJwt2Application.java
+++ src/main/java/com/hendisantika/springbootjwt2/SpringBootJwt2Application.java
@@ -1,7 +1,10 @@
 package com.hendisantika.springbootjwt2;
 
+import com.hendisantika.springbootjwt2.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootJwt2Application {
@@ -9,4 +12,13 @@ public class SpringBootJwt2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwt2Application.class, args);
 	}
+
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new JwtFilter());
+        registrationBean.addUrlPatterns("/secure/*");
+
+        return registrationBean;
+    }
 }
